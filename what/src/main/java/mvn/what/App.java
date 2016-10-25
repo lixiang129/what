675e161d95be2d1642803d1e0348f251f51ee496
Administrator <Administrator@AFOHP-610121041>
2016-10-25 22:15:30 +0800

    first commit

---
+++ what/src/main/java/mvn/what/App.java
@@ -0,0 +1,13 @@
+package mvn.what;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "你好吗" );
+    }
+}
---
+++ what/src/main/java/mvn/what/B.java
@@ -0,0 +1,8 @@
+package mvn.what;
+
+public class B {	public String website;
+public String content;
+public int counts;
+public int waitTime;
+public String[] login;
+}
